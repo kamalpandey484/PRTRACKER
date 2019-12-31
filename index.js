db12fca140c3ef9617fb6dda43868d21c01220b8
kamal <kamal.pandey@tothenew.com>
2019-12-31 15:12:26 +0530

    Changed cron job time

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 // Cron Jobs
 // eslint-disable-next-line no-new
-new CronJob('0 11 */1 * * *', () => {
+new CronJob('0 16 */1 * * *', () => {
   console.log('Cron job running!');
   postPullRequestData();
 }, null, true, 'Asia/Kolkata');
