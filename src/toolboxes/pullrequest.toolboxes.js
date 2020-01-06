cb2f27eddcf8b161b10203dbccc7b58b11194ae6
kamal <kamal.pandey@tothenew.com>
2020-01-06 15:37:58 +0530

    Changed rhinofront and rhinoapi PRs count.

--- src/toolboxes/pullrequest.toolboxes.js
+++ src/toolboxes/pullrequest.toolboxes.js
@@ -30,7 +30,7 @@ export const getComments = async (component, value) => {
 };
 
 export const getprcheck = async (perPage, state) => {
-  const getAllPrData = await Promise.all([getpr('rhinofront', perPage, state), getpr('rhinoapi', perPage, state), getpr('rhinostyle', perPage, state), getpr('rhinoaudit', 2, state), getpr('rhinopay', 2, state), getpr('rhinomatic', 2, state), getpr('rhinoaudit-client', 2, state), getpr('rhinotilities', 2, state), getpr('rhinocron', 2, state)])
+  const getAllPrData = await Promise.all([getpr('rhinofront', perPage, state), getpr('rhinoapi', perPage, state), getpr('rhinostyle', 5, state), getpr('rhinoaudit', 2, state), getpr('rhinopay', 2, state), getpr('rhinomatic', 2, state), getpr('rhinoaudit-client', 2, state), getpr('rhinotilities', 2, state), getpr('rhinocron', 2, state)])
     .then((res) => res)
     .catch((e) => e);
   return getAllPrData;
