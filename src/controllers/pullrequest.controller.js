826541644c5d6cc332d96faccd97ac511a0ec590
kamal <kamal.pandey@tothenew.com>
2020-01-06 16:00:56 +0530

    PR count changed for better performance

--- src/controllers/pullrequest.controller.js
+++ src/controllers/pullrequest.controller.js
@@ -6,7 +6,7 @@ import {
 } from '../db/services/pullrequest.service';
 
 export const postPullRequests = async (req, res) => {
-  await postPullRequestData(20);
+  await postPullRequestData(15);
   res.json('successfully done!');
 };
 
