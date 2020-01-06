9a1370b52dcb08cc0a858984b9929ba8bb928229
kamal <kamal.pandey@tothenew.com>
2020-01-06 16:22:42 +0530

    Changed pr count

--- src/toolboxes/pullrequest.toolboxes.js
+++ src/toolboxes/pullrequest.toolboxes.js
@@ -30,7 +30,7 @@ export const getComments = async (component, value) => {
 };
 
 export const getprcheck = async (perPage, state) => {
-  const getAllPrData = await Promise.all([getpr('rhinofront', perPage, state), getpr('rhinoapi', perPage, state), getpr('rhinostyle', 5, state), getpr('rhinoaudit', 5, state), getpr('rhinopay', 3, state), getpr('rhinomatic', 5, state), getpr('rhinoaudit-client', 3, state), getpr('rhinotilities', 3, state), getpr('rhinocron', 3, state)])
+  const getAllPrData = await Promise.all([getpr('rhinofront', perPage, state), getpr('rhinoapi', perPage, state), getpr('rhinostyle', 5, state), getpr('rhinoaudit', 5, state), getpr('rhinopay', 3, state), getpr('rhinomatic', 15, state), getpr('rhinoaudit-client', 3, state), getpr('rhinotilities', 3, state), getpr('rhinocron', 3, state)])
     .then((res) => res)
     .catch((e) => e);
   return getAllPrData;
